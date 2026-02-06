b54ef81919a988b870f1bdfa7be85cc7f2f0bbf4
ilya.kazakov <ilya.kazakov@indriver.com>
2026-02-06 23:22:24 +0200

    Added removing of textView and save state when changing configuration

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task009Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task009Test.kt
@@ -16,7 +16,6 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Matchers.allOf
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,7 +32,7 @@ class Task009Test {
             allOf(
                 isAssignableFrom(TextView::class.java),
                 withId(R.id.titleTextView),
-                withText("Hello World!"),
+                withText(R.string.hello_world),
                 withParent(isAssignableFrom(LinearLayout::class.java)),
                 withParent(withId(R.id.rootLayout))
             )
@@ -42,7 +41,7 @@ class Task009Test {
         onView(
             allOf(
                 withId(R.id.removeButton),
-                withText("remove"),
+                withText(R.string.remove),
                 isAssignableFrom(Button::class.java),
                 withParent(isAssignableFrom(LinearLayout::class.java)),
                 withParent(withId(R.id.rootLayout))
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,36 +2,40 @@ package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
+import androidx.core.view.contains
 
 class MainActivity : AppCompatActivity() {
+    private val rootLayout: LinearLayout by lazy { findViewById(R.id.rootLayout) }
     private val titleTextView: TextView by lazy { findViewById(R.id.titleTextView) }
-    private val hideButton: Button by lazy { findViewById(R.id.hideButton) }
+    private val removeButton: Button by lazy { findViewById(R.id.removeButton) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        hideButton.setOnClickListener {
-            titleTextView.isVisible = false
+        removeButton.setOnClickListener {
+            rootLayout.removeView(titleTextView)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putBoolean(TITLE_KEY, titleTextView.isVisible)
+        outState.putBoolean(IS_TEXT_VIEW_REMOVED_KEY, !rootLayout.contains(titleTextView))
     }
 
     override fun onRestoreInstanceState(
         savedInstanceState: Bundle
     ) {
         super.onRestoreInstanceState(savedInstanceState)
-        titleTextView.isVisible = savedInstanceState.getBoolean(TITLE_KEY)
+        if (savedInstanceState.getBoolean(IS_TEXT_VIEW_REMOVED_KEY)) {
+            rootLayout.removeView(titleTextView)
+        }
     }
 
     companion object {
-        const val TITLE_KEY = "TITLE_KEY"
+        const val IS_TEXT_VIEW_REMOVED_KEY = "IS_TEXT_VIEW_REMOVED_KEY"
     }
 }
