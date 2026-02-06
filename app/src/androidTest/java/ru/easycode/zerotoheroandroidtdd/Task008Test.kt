bf7afb01a6a3bd4c2338f4d21d6167333f588536
ilya.kazakov <ilya.kazakov@indriver.com>
2026-02-06 23:12:40 +0200

    Added saving of textView visibility when changing configuration

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task008Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task008Test.kt
@@ -16,7 +16,6 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.not
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,7 +32,7 @@ class Task008Test {
             allOf(
                 isAssignableFrom(TextView::class.java),
                 withId(R.id.titleTextView),
-                withText("Hello World!"),
+                withText(R.string.hello_world),
                 withParent(isAssignableFrom(LinearLayout::class.java)),
                 withParent(withId(R.id.rootLayout))
             )
@@ -42,7 +41,7 @@ class Task008Test {
         onView(
             allOf(
                 withId(R.id.hideButton),
-                withText("hide"),
+                withText(R.string.hide),
                 isAssignableFrom(Button::class.java),
                 withParent(isAssignableFrom(LinearLayout::class.java)),
                 withParent(withId(R.id.rootLayout))
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -4,33 +4,34 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     private val titleTextView: TextView by lazy { findViewById(R.id.titleTextView) }
-    private val changeButton: Button by lazy { findViewById(R.id.changeButton) }
+    private val hideButton: Button by lazy { findViewById(R.id.hideButton) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        changeButton.setOnClickListener {
-            titleTextView.text = getString(R.string.i_am_an_android_developer)
+        hideButton.setOnClickListener {
+            titleTextView.isVisible = false
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(TITLE_KEY, titleTextView.text.toString())
+        outState.putBoolean(TITLE_KEY, titleTextView.isVisible)
     }
 
     override fun onRestoreInstanceState(
         savedInstanceState: Bundle
     ) {
         super.onRestoreInstanceState(savedInstanceState)
-        titleTextView.text = savedInstanceState.getString(TITLE_KEY)
+        titleTextView.isVisible = savedInstanceState.getBoolean(TITLE_KEY)
     }
 
     companion object {
-        const val TITLE_KEY = "title"
+        const val TITLE_KEY = "TITLE_KEY"
     }
 }
