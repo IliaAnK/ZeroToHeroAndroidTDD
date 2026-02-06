9754622a8444eb63c23731da04e0d1c0f440e6ee
ilya.kazakov <ilya.kazakov@indriver.com>
2026-02-06 23:00:25 +0200

    Added changing of text after click to change button

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task006Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task006Test.kt
@@ -5,13 +5,16 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.PositionAssertions
+import androidx.test.espresso.assertion.PositionAssertions.isCompletelyBelow
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Matchers.allOf
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -28,7 +31,7 @@ class Task006Test {
             allOf(
                 isAssignableFrom(TextView::class.java),
                 withId(R.id.titleTextView),
-                withText("Hello World!"),
+                withText(R.string.hello_world),
                 withParent(isAssignableFrom(LinearLayout::class.java)),
                 withParent(withId(R.id.rootLayout))
             )
@@ -37,14 +40,14 @@ class Task006Test {
         onView(
             allOf(
                 withId(R.id.changeButton),
-                withText("change"),
+                withText(R.string.change),
                 isAssignableFrom(Button::class.java),
                 withParent(isAssignableFrom(LinearLayout::class.java)),
                 withParent(withId(R.id.rootLayout))
             )
-        ).check(PositionAssertions.isCompletelyBelow(withId(R.id.titleTextView)))
+        ).check(isCompletelyBelow(withId(R.id.titleTextView)))
 
         onView(withId(R.id.changeButton)).perform(click())
-        onView(withId(R.id.titleTextView)).check(matches(withText("I am an Android Developer!")))
+        onView(withId(R.id.titleTextView)).check(matches(withText(R.string.i_am_an_android_developer)))
     }
 }
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val titleTextView: TextView by lazy { findViewById(R.id.titleTextView) }
+    private val changeButton: Button by lazy { findViewById(R.id.changeButton) }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        changeButton.setOnClickListener {
+            titleTextView.text = getString(R.string.i_am_an_android_developer)
+        }
     }
 }
