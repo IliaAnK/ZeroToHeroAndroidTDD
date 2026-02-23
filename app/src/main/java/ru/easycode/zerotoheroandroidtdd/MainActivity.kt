b635c327ef944b929d5391b22a9bcf97fd35cc37
ilya.kazakov <ilya.kazakov@indriver.com>
2026-02-23 10:00:59 +0200

    Forgot about UI test. Now all solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -9,14 +10,42 @@ class MainActivity : AppCompatActivity() {
     private val countTextView: TextView by lazy { findViewById(R.id.countTextView) }
     private val incrementButton: Button by lazy { findViewById(R.id.incrementButton) }
 
-    private var count: Count = Count.Base(step = 2, max = 10)
+    private lateinit var state: UiState
+    private var count: Count = Count.Base(step = 2, max = 5)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        state = UiState.Initial(getString(R.string.zero))
+
         incrementButton.setOnClickListener {
-            countTextView.text = count.increment(countTextView.text.toString()).text
+            changeStateTo(count.increment(countTextView.text.toString()))
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(STATE_KEY, state)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val restoredState = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            savedInstanceState.getSerializable(STATE_KEY, UiState::class.java) as UiState
+        } else {
+            savedInstanceState.getSerializable(STATE_KEY) as UiState
+        }
+
+        changeStateTo(restoredState)
+    }
+
+    private fun changeStateTo(state: UiState) {
+        this.state = state
+        state.apply(countTextView, incrementButton)
+    }
+
+    companion object {
+        const val STATE_KEY = "STATE_KEY"
     }
 }
